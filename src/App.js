8b90a932421364d945b1387f1b2016bbe4962b88
Nikita Jiandani <niki.jiandani@gmail.com>
2019-08-22 15:16:20 -0400

    Add search feature

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
 import React from "react";
 import "./App.css";
 import { useFetch } from "./useFetch.js";
+import Search from "./components/Search.js";
+
+const handleChange = e => {
+  e.preventDefault();
+  let query = e.target.value.toUpperCase();
+  let students = document.getElementsByClassName("student");
+  for (var i = 0; i < students.length; i++) {
+    students[i]
+      .getElementsByTagName("h3")[0]
+      .innerText.toUpperCase()
+      .includes(query)
+      ? (students[i].style.display = "")
+      : (students[i].style.display = "none");
+  }
+};
 
 function StudentList(props) {
   return (
-    <ul className="student-list">
-      {props.data.students.map(item => (
-        <li key={item.id} className="student">
-          <img src={item.pic} alt="avatar" />
-          <section>
-            <h3>
-              {item.firstName} {item.lastName}
-            </h3>
-            <div>
-              <p>Email: {item.email}</p>
-              <p>Company: {item.company}</p>
-              <p>Skill: {item.skill}</p>
-              <p>
-                Average:
-                {item.grades.reduce((a, b) => Number(a) + Number(b)) /
-                  item.grades.length}
-                %
-              </p>
-            </div>
-          </section>
-        </li>
-      ))}
-    </ul>
+    <main className="student-list">
+      <Search handleChange={handleChange} />
+      <ul>
+        {props.data.students.map(item => (
+          <li key={item.id} className="student">
+            <img src={item.pic} alt="avatar" />
+            <section>
+              <h3 className="student-name">
+                {item.firstName} {item.lastName}
+              </h3>
+              <div>
+                <p>Email: {item.email}</p>
+                <p>Company: {item.company}</p>
+                <p>Skill: {item.skill}</p>
+                <p>
+                  Average:
+                  {item.grades.reduce((a, b) => Number(a) + Number(b)) /
+                    item.grades.length}
+                  %
+                </p>
+              </div>
+            </section>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
 
